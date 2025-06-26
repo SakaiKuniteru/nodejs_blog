1d0727af4310ee756d12973baace61efa66b95b8
Quốc Huy <quochuyks2k3@gmail.com>
2025-06-26 18:29:48 +0700

    create and edit

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,5 +1,7 @@
 const Course = require('../models/Course');
 const { mongooseToObject } = require('../../util/mongoose')
+const slugify = require('slugify');
+const shortid = require('shortid');
 
 class CourseController {
 
@@ -11,6 +13,41 @@ class CourseController {
                 course : mongooseToObject(course)
             });
         })
+        .catch(next);
+    }
+
+    // [GET] /courses/create
+    create(req, res, next) {
+        res.render("courses/create");
+    }
+
+    // [POST] /courses/store
+    async store(req, res, next) { 
+    try {
+        const formData = req.body;
+        formData.image = `https://img.youtube.com/vi/${req.body.videoID}/sddefault.jpg`;
+        formData.slug = `${slugify(formData.name, { lower: true, strict: true })}-${shortid.generate()}`; // Slug duy nhất
+        const course = new Course(formData);
+        await course.save();
+        res.redirect('/');
+        } catch (err) {
+        next(err);
+        }
+    }
+
+    // [GET] /courses/:id/edit
+    edit(req, res, next) {
+        Course.findById(req.params.id)
+        .then(course => res.render("courses/edit", {
+            course: mongooseToObject(course)
+        }))
+        .catch(next);
+    }
+
+    // [PUT] /courses/:id
+    update(req, res, next){
+        Course.updateOne({ _id: req.params.id }, req.body)
+        .then(() => res.redirect('/me/stored/courses'))
         .catch(next);
     }
 }
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: { type: String, maxLength: 255 },
+    name: { type: String, required: true },
     description: { type: String, maxLength: 600 },
     image: { type: String, maxLength: 255 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-})
+    videoID: { type: String, required: true },
+    level: { type: String, maxLength: 255 },
+    slug: { type: String, unique: true } // unique để slug không bị trình lặp
+}, {
+    timestamps: true,
+});
 
 module.exports = mongoose.model('Course', Course)
