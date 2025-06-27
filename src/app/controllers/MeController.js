c79b433d7ab582b153e239d8a006f6565fdcc3d6
Quốc Huy <quochuyks2k3@gmail.com>
2025-06-28 00:20:41 +0700

    Sort middleware

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,7 +5,15 @@ class MeController {
 
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-      Course.find({})
+      let courseQuery = Course.find({});
+
+      if (req.query.hasOwnProperty('_sort')) {
+        courseQuery = courseQuery.sort({
+          [req.query.column ]: req.query.type
+        });
+      }
+
+      courseQuery
         .then(courses => res.render('me/stored-courses', {
           courses: muntipleMongooseToObject(courses)
         }))
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
+
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
+
 const { request } = require('http');
 const app = express();
 const port = 3000;
 
 const route = require('./routes');
 const db = require('./config/db');
+const { type } = require('os');
 
 //connect to DB
 db.connect();
@@ -24,6 +28,25 @@ app.use(express.json());
 
 app.use(methodOverride('_method'));
 
+app.use(SortMiddleware);
+
+app.get('/middleware', 
+    function( req, res, next) {
+        if (['vethuong', 'vevip'].includes(req.query.ve)) {
+            req.face = 'Gach gach gach';
+            return next();
+        }
+        res.status(403).json({ 
+            message: "Access denied"
+        })
+    },
+    function (req, res, next) {
+    res.json({
+        message: 'Successfully!!!',
+        face: req.face
+    })
+})
+
 // HTTP Logger
 // app.use(morgan('combined'));
 
@@ -34,6 +57,30 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+
+                const icons = {
+                    default: 'bx bxs-sort-alt',
+                    asc: 'bx bx-sort-up',
+                    desc: 'bx bx-sort-down',
+                };
+
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc'
+                }
+
+                const icon = icons[sortType];
+                const type = types[sortType];
+
+
+                return `<a href="?_sort&column=${field}&type=${type}">
+                    <i class='${icon}' ></i>
+                </a>`;
+            }
+            
         }
     }),
 );
